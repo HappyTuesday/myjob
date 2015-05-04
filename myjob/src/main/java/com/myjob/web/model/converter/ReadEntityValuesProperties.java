055daf004c0df02ca8772ceef613d16f7f14cf0d
morris1990 <morris1990@outlook.com>
2015-05-04 22:24:47 +0800

    Modify the readEntityValuesProperties method.

--- myjob/src/main/java/com/myjob/web/model/converter/ReadEntityValuesProperties.java
+++ myjob/src/main/java/com/myjob/web/model/converter/ReadEntityValuesProperties.java
@@ -10,19 +10,23 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ReadEntityValuesProperties {
+	private String temp;
+	private StringBuilder fileContent= new StringBuilder();
+	public ReadEntityValuesProperties() throws IOException{
+				BufferedReader br = new BufferedReader(
+						new InputStreamReader(
+								new FileInputStream(
+										new File("Folder/Chat/EntityValues.properties"))));
+		
+		while(null!=(temp=br.readLine())){
+		fileContent.append(temp);
+		}		
+		br.close();	
+	}
 	public String readEnityValuesProperties(String entityValue) throws IOException{
 		String temp=null;
-		BufferedReader br = new BufferedReader(
-								new InputStreamReader(
-										new FileInputStream(
-												new File("Folder/Chat/EntityValues.properties"))));
-		StringBuilder fileContent= new StringBuilder();
-		while(null!=(temp=br.readLine())){
-			fileContent.append(temp);
-		}
 		temp =fileContent.substring(fileContent.indexOf(entityValue)+entityValue.length());
-		temp=temp.substring(1,temp.indexOf("/"));
-		br.close();
+		temp=temp.substring(1,temp.indexOf("/"));	
 		return temp;	
 	}
 
